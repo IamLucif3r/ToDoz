569ed022c397670ad7aaeafa325d06b9049d59aa
Aayush Bangroo <aayush421999@gmail.com>
2021-10-01 12:37:04 +0530

    added template driven form and validations

--- src/app/MyComponents/add-todo/add-todo.component.ts
+++ src/app/MyComponents/add-todo/add-todo.component.ts
@@ -1,29 +1,22 @@
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { NgForm } from '@angular/forms';
 import { todo } from 'src/app/todo';
 
 @Component({
   selector: 'app-add-todo',
   templateUrl: './add-todo.component.html',
-  styleUrls: ['./add-todo.component.css']
+  styleUrls: ['./add-todo.component.css'],
 })
 export class AddTodoComponent implements OnInit {
-
-  title:string;
-  desc:string;
   @Output() todoAdd: EventEmitter<todo> = new EventEmitter();
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-  onSubmit(){
-    const todo={
-      sno: 8,
-      title: this.title,
-      desc: this.desc,
-      active:true
-  }
-  this.todoAdd.emit(todo);
-}
+  ngOnInit(): void {}
+  onSubmit(form: NgForm) {
+    const todo = { sno: 8, ...form.value, active: true };
+    this.todoAdd.emit(todo);
 
+    form.reset();
+  }
 }
